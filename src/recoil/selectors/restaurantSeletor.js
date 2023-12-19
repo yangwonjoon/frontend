d1a6e7a2f6ec668ce26c7e1acdc78f5d90b5e314
yangwonjoon <diddnjswns20@gmail.com>
2023-12-20 02:42:05 +0900

    일단 로그인, 회원가입 제외 병합한거

--- src/recoil/selectors/restaurantSeletor.js
+++ src/recoil/selectors/restaurantSeletor.js
@@ -8,10 +8,7 @@ export const restaurantSelector = selector({
 
         try {
             //api호출
-            const res = await axios.get('http://localhost:3000/data/sample.json');
-
-
-
+            const res = await axios.get('http://localhost:8080/api/restaurant/all');
 
             return res.data;
         } catch (error) {
